5a55560bb3cef9079fec91c2ab2040f26535242b
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-07-17 14:57:58 +0400

    Guest's full name with validation

--- Guest.cs
+++ Guest.cs
@@ -9,31 +9,23 @@ namespace SimpleHotelRoomManagementProjectWithOOP
     // This class represents a Guest (visitor) in the hotel.
     class Guest
     {
-        public string Name { get; }  // Guest's full name
-        public string NationalID { get; }  // National ID for identity verification
-        public string RoomNumber { get; set; } // Room number for contact
-        public int Nights { get; set; } // Property to store the number of nights the guest will stay in the room
+        private string name; // Guest's full name
+        private string nationalID;  // National ID for identity verification
+        private string phoneNumber; // Room number for contact
 
-        // Constructor with validation for required fields
-        public Guest(string name, string nationalID, string roomNumber, int nights)
+        // Guest's full name with validation
+        public string Name
         {
-            if (string.IsNullOrWhiteSpace(name) || name.Length < 3)
-                throw new ArgumentException("Guest name must be at least 3 characters.");
-
-            if (string.IsNullOrWhiteSpace(nationalID) || nationalID.Length < 5)
-                throw new ArgumentException("National ID must be at least 5 characters.");
-
-            if (string.IsNullOrWhiteSpace(roomNumber))
-                throw new ArgumentException("Room number cannot be empty.");
-
-            if (nights <= 0)
-                throw new ArgumentException("Nights must be greater than 0.");
-
-            Name = name;
-            NationalID = nationalID;
-            RoomNumber = roomNumber;
-            Nights = nights;
+            get => name;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 3)
+                    throw new ArgumentException("Guest name must be at least 3 characters.");
+                name = value;
+            }
         }
+
+
 
         // Override ToString to display guest information clearly
         public override string ToString()
