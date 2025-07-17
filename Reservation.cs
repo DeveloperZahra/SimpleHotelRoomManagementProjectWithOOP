b3246b6f7854df7eb5acd0f686ddbf2c500a3a9d
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-07-17 14:48:15 +0400

    last update for class Reservation

--- Reservation.cs
+++ Reservation.cs
@@ -9,34 +9,28 @@ namespace SimpleHotelRoomManagementProjectWithOOP
     // This class represents a Reservation between a Guest and a Room.
     class Reservation
     {
-        public string  GuestName { get; }   // Guest name who mode the reservation
-
-        public string RoomNumber { get; set; } // Room number for contact
-
-        public int Nights { get; set; } // Property to store the number of nights the guest will stay in the room
-
-        public DateTime BookingDate { get; set; }  // Date of booking, stored at creation time 
-
+        public Guest GuestInfo { get; set; }      // The guest who made the reservation
+        public int Nights { get; set; }           // Number of nights reserved
+        public DateTime BookingDate { get; set; } // Date when reservation was made
 
 
         // Constructor ensures nights > 0 and marks the room as reserved
 
-        public  Reservation(string guestname , string RoomNo, int nights)
+        public  Reservation(Guest guest, int nights)
         {
-            if (nights <= 0)
-                throw new ArgumentException("Nights must be greater than 0.");
+            if (nights <= 0) // Validate that the number of nights is positive (cannot reserve 0 or negative nights)
+                throw new ArgumentException("Number of nights must be positive.");
 
-            GuestName = guestname;
-            RoomNumber = RoomNo;
-            Nights = nights;
-            BookingDate = DateTime.Now;
-            Room.IsReserved = true;
+            GuestInfo = guest; // Store the guest information (the guest who made the reservation)
+            Nights = nights; // Store the number of nights for this reservation
+            BookingDate = DateTime.Now; // Store the date and time when the booking was made (current date and time)
         }
 
 
-
-
-
+        public override string ToString()
+        {
+            return $"Guest: {GuestInfo.Name}, Phone: {GuestInfo.PhoneNumber}, Room: (Assigned Later), Nights: {Nights}";
+        }
 
 
 
