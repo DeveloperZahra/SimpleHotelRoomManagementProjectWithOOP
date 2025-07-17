98adeb040713b375a5ffce9ef2862ec48551e471
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-07-17 11:37:37 +0400

    This class represents a Reservation between a Guest and a Room.

---
+++ Reservation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHotelRoomManagementProjectWithOOP
+{
+    // This class represents a Reservation between a Guest and a Room.
+    class Reservation
+    {
+        public string  GuestName { get; }   // Guest name who mode the reservation
+
+        public string RoomNumber { get; set; } // Room number for contact
+
+        public int Nights { get; set; } // Property to store the number of nights the guest will stay in the room
+
+        public DateTime BookingDate { get; set; }  // Date of booking, stored at creation time 
+    }
+}
