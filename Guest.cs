c5ae28828516bd437b4ece6bf6e4a5110f95857d
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-07-17 10:13:08 +0400

    create class represents a Guest (visitor) in the hotel

---
+++ Guest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHotelRoomManagementProjectWithOOP
+{
+    // This class represents a Guest (visitor) in the hotel.
+    class Guest
+    {
+        public string Name { get; }
+        public string NationalID { get; }
+        public string RoomNumber { get; }
+        public int Nights { get; }
+    }
+}
