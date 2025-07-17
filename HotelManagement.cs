bfd27fcfdde74dc112010b07bc65de11530a0d87
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-07-17 15:20:43 +0400

    method to add a new guest to the guest list

--- HotelManagement.cs
+++ HotelManagement.cs
@@ -12,8 +12,14 @@ namespace SimpleHotelRoomManagementProjectWithOOP
         private List<Guest> guests = new List<Guest>();  // All guests
         private List<Room> rooms = new List<Room>();     // All rooms
 
+        // Method to add a new guest to the guests list
 
-
+        public void AddGuest(Guest guest)
+        {
+            if (guests.Any(g => g.NationalID == guest.NationalID)) // Check if there is already a guest with the same National ID in the list
+                throw new Exception("Guest with this National ID already exists."); // If found, throw an exception to prevent duplicate National ID
+            guests.Add(guest);  // If no duplicate is found, add the guest to the list
+        }
 
 
     }
