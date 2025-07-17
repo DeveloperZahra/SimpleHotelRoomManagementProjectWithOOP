472f7425b9ec2dfe918cad3c17c931c19767292f
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-07-17 15:06:57 +0400

    Manages guests, rooms, and reservations

---
+++ HotelManagement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHotelRoomManagementProjectWithOOP
+{
+    // Manages guests, rooms, and reservations
+    class HotelManagement
+    {
+        private List<Guest> guests = new List<Guest>();  // All guests
+        private List<Room> rooms = new List<Room>();     // All rooms
+
+
+
+
+
+    }
+}
