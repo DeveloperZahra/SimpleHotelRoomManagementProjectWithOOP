b94d677bd5e1cc825a1d7c5462d5dab532512591
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-07-17 09:57:07 +0400

    add a coment into class room

--- Room.cs
+++ Room.cs
@@ -14,21 +14,21 @@ namespace SimpleHotelRoomManagementProjectWithOOP
         public double DailyRate  { get; set; } // Property to store the daily rate of the room.
 
         // Overrides the default ToString method to return a formatted string with room information.
-        public override string ToString()
+        public override string ToString() //ToString()-> Returns a readable string about the room
         {
 
             return $"Room RoomNumber: {RoomNumber}, DailyRate: {DailyRate}"; // Returns a string showing the room number and daily rate.
         }
 
         // Static method to create a Room object from a string.
-        public static Room FromString(string line)
+        public static Room FromString(string line) //FromString()-> Converts a text line into a Room object
 
         {
             var parts = line.Split('|'); // Split the input string using the '|' character as a separator.
             return new Room  // Create a new Room object and assign the values after parsing
             {
                 RoomNumber = int.Parse(parts[0]), // Convert the first part to int for the room number.
-                DailyRate = int.Parse(parts[1]) // Convert the second part to double for the daily rate.
+                DailyRate = double.Parse(parts[1]) // Convert the second part to double for the daily rate.
                                                 // It's better to use double.Parse instead of int.Parse here.
             };
 
