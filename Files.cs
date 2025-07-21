73c092186574bec591610440d3afd24ecb4ba627
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-07-21 08:31:37 +0400

    add Load guests from a text file

--- Files.cs
+++ Files.cs
@@ -21,6 +21,36 @@ namespace SimpleHotelRoomManagementProjectWithOOP
                 }
             }
         }
+
+
+
+        // Load guests from a text file
+        public static List<Guest> LoadGuests(string filePath)
+        {
+            List<Guest> guests = new List<Guest>();
+            if (!File.Exists(filePath)) return guests;
+
+            var lines = File.ReadAllLines(filePath);
+            foreach (var line in lines)
+            {
+                var parts = line.Split('|');
+                guests.Add(new Guest(parts[0], parts[1], parts[2]));
+            }
+            return guests;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
 
