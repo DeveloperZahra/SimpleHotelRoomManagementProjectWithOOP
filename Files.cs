c5a217b17c878d263d3726900f704cc416d2fa9d
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-07-21 08:29:50 +0400

    add Save guests to a text file

---
+++ Files.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHotelRoomManagementProjectWithOOP
+{
+    //__________This class includes save and load files__________ 
+    internal class Files
+    {
+
+        // Save guests to a text file
+        public static void SaveGuests(List<Guest> guests, string filePath)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                foreach (var guest in guests)
+                {
+                    writer.WriteLine($"{guest.Name}|{guest.PhoneNumber}|{guest.NationalID}");
+                }
+            }
+        }
+    }
+}
+
