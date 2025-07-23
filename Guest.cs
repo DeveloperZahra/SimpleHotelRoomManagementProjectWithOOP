8551bebf523bb0962c9bbaa87aafe151e75f68a6
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-07-23 09:09:13 +0400

    Added the load & save file

--- Guest.cs
+++ Guest.cs
@@ -67,5 +67,35 @@ namespace SimpleHotelRoomManagementProjectWithOOP
         }
 
 
+
+
+        // Load guests from a text file
+        public static List<Guest> LoadGuests(string filePath)
+        {
+            List<Guest> guests = new List<Guest>();
+            if (!File.Exists(filePath)) return guests;
+
+            var lines = File.ReadAllLines(filePath);
+            foreach (var line in lines)
+            {
+                var parts = line.Split('|');
+                guests.Add(new Guest(parts[0], parts[1], parts[2]));
+            }
+            return guests;
+        }
+
+        // Save guests to a text file
+        public static void SaveGuests(List<Guest> guests, string filePath)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                foreach (var guest in guests)
+                {
+                    writer.WriteLine($"{guest.Name}|{guest.PhoneNumber}|{guest.NationalID}");
+                }
+            }
+        }
+
+
     }
 }
